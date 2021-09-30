84dcd7273bfcfbcee71df99cb225615b0efcef9b
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2021-09-30 10:18:18 +0700

    Update #1: Complete 'Build' module for FabledWand

--- Fabled.js
+++ Fabled.js
@@ -1,4 +1,5 @@
 var Player = BukkitPlayer;
+var World = Player.getWorld();
 var Server = BukkitServer;
 var Manager = Server.getPluginManager();
 var Plugin = Manager.getPlugin("PlaceholderAPI");
@@ -26,19 +27,19 @@ function FabledData(database, gifts, uses) {
   this.getUses = function() return this.uses;
   this.setUses = function(param) {
     if(isNaN(param) || param < 0)
-      throw ChatColor[Colors[0]]('&', "&cLỗi: &fSố điền vào không hợp lệ!");
+      throw "&cLỗi: &fSố điền vào không hợp lệ!";
     else this.uses = param; // set value
   };
   this.getDatabase = function() return this.database;
   this.setDatabase = function() return; // doesn't allow to modify table layout
   this.setValue = function(key, value) {
     if(!this.database.keySet().contains(key))
-      throw ChatColor[Colors[0]]('&', "&cLỗi: &fLoại khoáng sản không hợp lệ!");
+      throw "&cLỗi: &fLoại khoáng sản không hợp lệ!"
     else this.database.put(key, value); // update table
   };
   this.addToStorage = function(key, amount) {
     if(!this.database.keySet().contains(key))
-      throw ChatColor[Colors[0]]('&', "&cLỗi: &fLoại khoáng sản không hợp lệ!");
+      throw "&cLỗi: &fLoại khoáng sản không hợp lệ!"
     else {
       var current = this.database.get(key); // get current data value
       this.database.put(key, Math.floor(key+current));
@@ -46,11 +47,11 @@ function FabledData(database, gifts, uses) {
   };
   this.removeFromStorage = function(key, amount) {
     if(!this.database.keySet().contains(key))
-      throw ChatColor[Colors[0]]('&', "&cLỗi: &fLoại khoáng sản không hợp lệ!");
+      throw "&cLỗi: &fLoại khoáng sản không hợp lệ!"
     else {
       var current = this.database.get(key); // current amount;
       if(current < amount)
-        throw ChatColor[Colors[0]]('&', "&cLỗi: &fKho của đũa không đủ khoáng sản!");
+        throw "&cLỗi: &fKho của đũa không đủ khoáng sản!"
       else this.database.put(key, current-amount);
     }
   };
@@ -63,27 +64,112 @@ function FabledData(database, gifts, uses) {
   this.addToGifted = function(receiver) this.gifts.add(receiver.getName());
 };
 
+// Object responsible for global functions
+var Script = {
+  formatNumber: function(num) {
+     num = num.toString();
+     var pattern = /(-?\d+)(\d{3})/;
+     while (pattern.test(num))
+         num = num.replace(pattern, "$1,$2");
+     return num;
+  },
+  createData: function(p) {
+    var dataMap = new HashMap();
+    dataMap.put("IRON", 0);
+    dataMap.put("GOLD", 0);
+    dataMap.put("DIAMOND", 0);
+    dataMap.put("EMERALD", 0);
+    var dataInstance = new FabledData(dataMap, new ArrayList(), 0);
+    p.setMetadata("fabledData", new FixedMetadataValue(Plugin, dataInstance));
+  };
+  getIgnoreBlocks: function() {
+    var ignore_set = new HashSet;
+    ignore_set.add(Material.WATER);
+    ignore_set.add(Material.STATIONARY_WATER);
+    ignore_set.add(Material.LAVA);
+    ignore_set.add(Material.STATIONARY_LAVA);
+    ignore_set.add(Material.AIR);
+    return ignore_set;
+  };
+  getDataDirectory: function(key) {
+    var metadata = Player.getMetadata("fabledData").get(0).value();
+    return metadata.getDatabase().get(key);
+  };
+  getTranslatedName: function(key) {
+    switch(key.toLowerCase()) {
+      case "iron": return "&fKhối Sắt";
+      case "gold": return "&6Khối Vàng";
+      case "diamond": return "&bKhối Kim Cương";
+      case "emerald": return "&aKhối Lục Bảo";
+      default: throw "&cLỗi: &fLoại khoáng sản không hợp lệ!";
+    }
+  }
+};
+
 // Main code for the wand begins here :)
 function FabledCore() {
   try {
     if(Fabled == null || LuckPerms == null)
       throw ChatColor[Colors[0]]('&', "&cLỗi: &fMáy chủ chưa cài đặt &aFabledSkyblock &fhoặc &aLuckPerms&f!");
     else {
-      var IslandManager = Fabled.getIslandManager(); var StackManager = Fabled.getStackableManager();
+      var IslandManager = Fabled.getIslandManager(); var StackManager = Fabled.getStackableManager(); var LevelManager = Fabled.getLevellingManager();
       var TargetIsland = IslandManager.getIsland(Player);
+      var fabledPlayerData = Player.getMetadata("fabledData").get(0).value();
       switch(args[0].toLowerCase()) {
-        case "build": /* coming soon */ break;
+        case "build":
+          if(!Player.hasMetadata("fabledData")) {
+            Player.sendMessage(ChatColor[Colors[0]]('&',
+              "&eFabled &8&l| &cLỗi: &fBạn không có quyền sử dùng vật phẩm này!"));
+            return -1;
+          } else {
+            var ignore_blocks = Script.getIgnoreBlocks(); var target = Player.getTargetBlock(ignore_blocks, 5);
+            if(!IslandManager.getIslandAtLocation(target.getLocation()).equals(TargetIsland)) {
+              Player.sendMessage(ChatColor[Colors[0]]('&',
+                "&eFabled &8&l| &cLỗi: &fBạn chỉ có thể dùng đũa ở đảo của mình!"));
+              return -1;
+            }
+            var type_key = target.getType().name().replace("_BLOCK", "");
+            if(!fabledPlayerData.getDatabase().keySet().contains(type_key)) {
+              Player.sendMessage(ChatColor[Colors[0]]('&',
+                "&eFabled &8&l| &cLỗi: &fLoại khối nhắm vào không hợp lệ!"));
+              return 1;
+            }
+            var balance = Script.getDataDirectory(type_key);
+            if(balance < 1) {
+              Player.sendMessage(ChatColor[Colors[0]]('&',
+                "&eFabled &8&l| &cLỗi: &fTrong kho khoáng sản không đủ vật phẩm!"));
+            } else {
+              var Build = Java.extend(Runnable {
+                run: function() {
+                  var aimed = StackManager.getStacks().get(target.getLocation());
+                  aimed.setSize(aimed.getSize() + balance); LevelManager.startScan(Player, TargetIsland);
+                  fabledPlayerData.setValue(type_key, 0);
+                }
+              }); var start = System.nanoTime(); Scheduler.runTask(Plugin, new Build()); var end = System.nanoTime();
+              var formatter = new DecimalFormat("#0.0"); var time = formatter.format((end-start)/1000000);
+              Player.sendMessage(ChatColor[Colors[0]]('&',
+                "&eFabled &8&l| &fĐã đặt thành công %a %b&f. Thời gian triển khai: %t")
+              .replace("%a", Script.formatNumber(balance))
+              .replace("%b", Script.getTranslatedName(type_key))
+              .repalce("%t", time)); return -1;
+            }
+          }
+          break;
         case "data": /* coming soon */ break;
+        case "is-fabled-user": /* maybe l8r */ break;
         case "add": /* coming soon */ break;
         case "remove": /* coming soon */ break;
         case "deposit": /* coming soon */ break;
         case "withdraw": /* coming soon */ break;
+        case "purchase": /* yea l8r */ break;
         case "gift": /* coming soon */ break;
         default: throw ChatColor[Colors[0]]('&', "&cLỗi: &fCú pháp lệnh không tồn tại!");
       }
     }
   } catch(err) {
     return "&eScript &8&l| " + err.message;
+  } finally {
+    Player.setMetadata("fabledData", fabledPlayerData);
   }
 }
 FabledCore();
