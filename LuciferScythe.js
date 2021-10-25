41edee18192acb30c1213d661f572aa8d5a7beb6
Stella (DucTrader) <63104132+emnoigi1452@users.noreply.github.com>
2021-10-25 09:02:59 +0700

    I'm bored, might as well update Lucifer for fun!

--- LuciferScythe.js
+++ LuciferScythe.js
@@ -1,44 +1,79 @@
-var executor = BukkitPlayer; var server = BukkitServer;
+var Executor = BukkitPlayer;
+var World = Executor.getWorld();
+var Server = BukkitServer;
+var MyItems = Server.getPluginManager().getPlugin("MyItems");
+var Scheduler = Server.getScheduler();
+var Host = Server.getPluginManager.getPlugin("PlaceholderAPI");
+
+var Runnable = Java.type("java.lang.Runnable");
 var ArrayList = Java.type("java.util.ArrayList");
+var ChatColor = org.bukkit.ChatColor;
+var EntityType = org.bukkit.entity.EntityType;
+var Statistic = org.bukkit.Statistic;
+var PotionEffect = org.bukkit.potion.PotionEffect;
+var PotionEffectType = org.bukkit.potion.PotionEffectType;
 
-function not_in_pvp(player) {
-	if(player.getWorld().getName() != "world") return true;
-	var region = "worldguard_region_name"; var loc = player.getLocation();
-	if(PlaceholderAPI.static.setPlaceholders(player, "%" + region + "%") == "nopvp")
-		return true;
-	else {
-		var x = loc.getBlockX(); var z = loc.getBlockZ();
-		return x_array.indexOf(x) != -1 && z_array.indexOf(z) != -1;
-	}
-}
+var Script = {
+   Placeholder: "%worldguard_region_name%";
+   getRadius: function(level) {
+      if(typeof level != "int")
+         throw "Loại dữ liệu không hợp lệ!";
+      else return level * 25;
+   },
+   getNearbyPlayers: function(radii) {
+      var members = Server.getOnlinePlayers(); var match = new ArrayList();
+      members.stream().filter(function(player) {
+         var reg = PlaceholderAPI.static.setPlaceholders(player, this.Placeholder);
+         return player.getWorld().equals(World) && reg == "pvp" && !player.equals(Executor) &&
+                && player.getLocation().distance(Executor.getLocation()) <= radii;
+      }).forEach(function(same) {
+         match.add(same);
+      }); return match;
+   },
+   getCalculatedStats: function(base, addition) {
+      return Math.floor(base * (1 + (addition / 100)));
+   },
+};
 
-function core_func() {
-	var tier = parseInt(args[0]);
-	if(isNaN(tier)) return "&eLucifer &8&l| &cLỗi: &fCấp độ skill không phù hợp";
-	else if(tier > 3 || tier < 1) return "&eLucifer &8&l| &cLỗi: &fSố hợp lệ chỉ nằm trong vùng &e1 - 3";
-	else {
-		var online_players = server.getOnlinePlayers(); var active = new ArrayList();
-		for(var j = 0; j < online_players.size(); j++) {
-			var player = online_players.get(j);
-			if(player.equals(executor)) continue;
-			if(player.getWorld().getName() != "world") continue;
-			else {
-				if(!not_in_pvp(player)) continue;
-				else {
-					if(player.getLocation().distance(executor.getLocation()) < (25*tier))
-						active.add(player);
-				}
-			}
-		} var victims = 0; var total_health = 0;
-		for(var x = 0; x < active.size(); x++) {
-			var victim = active.get(x);
-			if(victim.getHealth() < (30*tier)) {
-				total_health += victim.getHealth(); victims++;
-				var message = "&eLucifer &8&l| &c&oGiá như người mạnh mẽ hơn để bước vào lãnh địa của ta...";
-				victim.sendMessage(message.replace(/&/g, "§")); victim.setHealth(0);
-			}
-		}
-		return "&eLucifer &8&l| &fĐã tiêu diệt &e" + victims.toFixed() + " &fngười chơi &8&l[&a+" + total_health.toFixed(1) + "HP&8&l]"; 
-	}
+function main() {
+   try {
+      var level = parseInt(args[1]); 
+      if(isNaN(level) || level > 3 || level < 1)
+         throw "Cấp độ triển khai không hợp lệ!";
+      var StatsManager = MyItems.getPlayerManager().getPlayerItemStatsManager();
+      var Caster = StatsManager.getItemStatsWeapon(Executor);
+      var baseDamage = Caster.getTotalDamage(); var multiplier = Caster.getTotalPvPDamage();
+      var skillForce = Script.getCalculatedStats(baseDamage, multiplier);
+      var skillAreaOfEffect = Script.getRadius(level);
+      var Skill = Java.extend(Runnable {
+         run: function() {
+            var victims = 0; var gained = 0; var starters = Script.getNearbyPlayers(skillAreaOfEffect);
+            for each(var element in starters) {
+               var defenseStats = StatsManager.getItemStatsArmor(element); element.setFireTicks(20);
+               var baseDefense = defenseStats.getTotalDefense(); var addon = defenseStats.getTotalPvPDefense();
+               var blockade = Script.getCalculatedStats(baseDefense, addon);
+               if(blockade < skillForce) {
+                  victims++; gained += element.getHealth();
+                  var hellGate = element.getLocation(); element.setHealth(0);
+                  World.spawnEntity(hellGate, EntityType.EVOKER_FANGS);
+                  World.strikeLightningEffect(hellGate)
+                  var note = "&4Lucifer &8&l| &f&oMất hết ý chí chiến đấu, mất hết sức mạnh của mình...";
+                  element.sendMessage(ChatColor.translateAlternateColorCodes('&', note));
+               } else {
+                  var effectShock = new PotionEffect(PotionEffectType.BLINDNESS, 300, 5);
+                  element.damage(skillForce); effectShock.apply(element);
+                  element.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                     "&4Lucifer &8&l| &f&oKhá lắm...Ngươi đã chứng minh mình có khả năng..."));
+               }
+            }
+            Executor.incrementStatistic(Statistic.PLAYER_KILLS, victims);
+            Player.setMaxHealth(Math.floor(Player.getMaxHealth() + gained));
+            Player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+               "&4Lucifer &8&l| &a" + victims.toString() + " &fngười chết! Bạn được tăng &a+" + gained.toString() + "&fHP"));
+         }
+      }); Scheduler.runTask(Host, new Skill()); return 0;
+   } catch(err) {
+      return "&4Lucifer &8&l| &cLỗi: &f" + err.message;
+   }
 }
-core_func();
+main();
