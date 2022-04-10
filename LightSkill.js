f13fbe1c163572ca55e9a6fd29f2930bba9caa13
Stella (DucTrader) <63104132+emnoigi1452@users.noreply.github.com>
2022-04-10 23:02:58 +0700

    Used pastebin like 30 times to update this :(

--- LightSkill.js
+++ LightSkill.js
@@ -1,110 +1,108 @@
-var Executor = BukkitPlayer;
-var World = Executor.getWorld();
+var Player = BukkitPlayer;
+var World = Player.getWorld();
 var Server = BukkitServer;
-var MyItems = Server.getPluginManager().getPlugin("MyItems");
 var Scheduler = Server.getScheduler();
-var Host = Server.getPluginManager.getPlugin("PlaceholderAPI");
+var Manager = Server.getPluginManager();
+var MyItems = Manager.getPlugin("MyItems");
+var PvPManager = Manager.getPlugin("PvPManager");
+var Host = Manager.getPlugin("PlaceholderAPI");
+var DevMode = Player.getName() != "DucTrader";
 
-var Runnable = Java.type("java.lang.Runnable");
 var ArrayList = Java.type("java.util.ArrayList");
+var HashMap = Java.type("java.util.HashMap");
+var Runnable = Java.type("java.lang.Runnable");
+
 var ChatColor = org.bukkit.ChatColor;
-var Statistic = org.bukkit.Statistic;
+var Vector = org.bukkit.util.Vector;
+var Location = org.bukkit.Location;
+var Particle = org.bukkit.Particle;
 var PotionEffect = org.bukkit.potion.PotionEffect;
 var PotionEffectType = org.bukkit.potion.PotionEffectType;
-var Vector = org.bukkit.util.Vector;
 
-var Script = {
-   Region: '%worldguard_region_name%',
-   Envoy: '%crazyenvoy_crates_left%',
-   getCalculatedValue: function(base, addon) {
-      return base * (1 + (addon / 100));
-   },
-   getNearbyTargets: function(radius) {
-      var online = Server.getOnlinePlayers(); var matches = new ArrayList();
-      online.stream().filter(function(player) {
-         if(!World.equals(player.getWorld()))
-            return false;
-         else {
-            var region = PlaceholderAPI.static.setPlaceholders(player, this.Region);
-            var distance = player.getLocation().distance(Executor.getLocation());
-            return region == "pvp" && distance < radius;
-         }
-      }).forEach(function(pass) {
-         matches.add(pass);
-      }); return matches;
-   },
-   getLocationDamage: function(radius, distance, power) {
-      var constantValue = Math.floor(power / radius);
-      return distance < 3 ? power : constantValue * distance;
-   },
-   isInView: function(source, target) {
-      var direction = source.getDirection();
-      var source_vec = source.toVector();
-      var target_vec = target.toVector();
-      var dif = target.subtract(source).normalize();
-      return direction.dot(dif) > 0.25;
-   }
-};
+var Light = {
+  _SKILL_RADIUS_: 30,
+  checkPvPStatus: function(target, pvpInstance) {
+    var FlagCheck = "worldguard_region_flags";
+    var n1 = PlaceholderAPI.static.setPlaceholders(target, 
+      "%worldguard_region_flags%").contains("pvp=ALLOW");
+    var n2 = pvpInstance.hasPvPEnabled() && (!pvpInstance.isNewbie());
+    return n1 && n2;
+  },
+  color: function(param) {
+    return ChatColor.translateAlternateColorCodes('&', param);
+  },
+  handleEffect: function(type, location, density) {
+    var Altitude = parseInt(location.getBlockY() + 100);
+    var X = location.getBlockX(); var Z = location.getBlockZ();
+    for(; Altitude >= location.getBlockY(); Altitude--)
+      World.spawnParticle(type, new Location(World, X, Altitude, Z), density);
+  },
+  checkSighting: function(target) {
+    // Algorithm by HorseNuggets :3 tks
+    var dir = Player.getLocation().getDirection();
+    var v1 = Player.getLocation().toVector();
+    var v2 = target.getLocation().toVector();
+    var dif = v2.subtract(v1).normalize();
+    return dir.dot(dif) < 0.25;
+  },
+  cdist: function(damage, distance) {
+    var formula = 100 - (2.5 * distance);
+    return Math.floor(damage * (formula / 100));
+  },
+  calc: function(base, inc) {
+    return Math.floor(base * (inc / 100));
+  }
+}
 
 function main() {
-   try {
-      var crateStatus = parseInt(PlaceholderAPI.static.setPlaceholders(Executor, Script.Envoy));
-      if(!isNaN(crateStatus) && crateStatus >= 12)
-         throw "&oBầu không khí có nhiều chướng ngại vật...Không thể triển khai phép!";
-      var StatsManager = MyItems.getPlayerManager().getPlayerItemStatsManager();
-      var LoreStats = MyItems.getGameManager().getLoreStatsManager();
-      if(p.getHealth() > 400 || p.getMaxHealth() > 500) {
-         var healthCap = Executor.getHealth() > 1536 ? 1536 :  Executor.getHealth();
-         var skillRadius = Math.floor(Math.pow((healthCap / 15), Math.cos(102.4 * (Math.PI / 180))));
-         var playerStats = StatsManager.getItemStatsWeapon(Executor);
-         var baseDamage = Math.floor(playerStats.getTotalDamageMax()); var additionPvP = playerStats.getTotalPvPDamage();
-         var skillForce = Math.floor(Script.getCalculatedValue(baseDamage, additionPvP));
-         var ItemLoreStats = LoreStats.getLoreStatsWeapon(Executor.getInventory().getItemInMainHand());
-         var critChance = ItemLoreStats.getCriticalChance(); var randomization = Math.floor(Math.random() * 100) + 1;
-         var lightningStrike = randomization > critChance ? Math.floor(baseDamage * ItemLoreStats.getCriticalDamage()) : baseDamage;
-         var Phoebus = Java.extend(Runnable, {
-            run: function() {
-               var deceased = 0; var nearbyPlayers = Script.getNearbyTargets(skillRadius);
-               for each(var target in nearbyPlayers) {
-                  var blind = new PotionEffect(PotionEffectType.BLINDNESS, 300, 5);
-                  var weak = new PotionEffect(PotionEffectType.WEAKNESS, 600, 10);
-                  blind.apply(target); weak.apply(target);
-                  target.setVelocity(new Vector(0, 5, 0));
-                  if(Script.isInView(Executor.getLocation(), target.getLocation())) {
-                     World.strikeLightningEffect(target.getLocation());
-                     var targetStamina = target.getHealth(); var targetCapacity = target.getMaxHealth();
-                     if(lightningStrike > targetCapacity || lightningStrike > targetStamina) {
-                        target.setHealth(0); deceased++;
-                     } else {
-                        target.damage(lightningStrike); target.setFireTicks(300);
-                     }
-                  } else {
-                     var defenseStats = StatsManager.getItemStatsArmor(target);
-                     var baseDef = defenseStats.getTotalDefense(); var pvpBuff = defenseStats.getTotalPvPDefense();
-                     var totalDefense = Script.getCalculatedValue(baseDef, pvpBuff); var blockChance = ItemLoreStats.getBlockChance();
-                     var penetrationValue = ItemLoreStats.getPenetration();
-                     var blockAbility = blockChance > penetrationValue ? blockChance - penetrationValue : 0;
-                     totalDefense = Math.floor(totalDefense * (blockAbility / 100));
-                     var distanceFromCenter = target.getLocation().distance(Executor.getLocation());
-                     var burnDamage = Script.getLocationDamage(skillRadius, distanceFromCenter, skillForce) - totalDefense;
-                     if(burnDamage > Math.floor(target.getHealth())) {
-                        target.setHealth(0); deceased++;
-                     } else {
-                        target.damage(burnDamage); target.setFireTicks(600);
-                     }
-                  }
-                  target.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                     "&6Light &8&l| &f&oMột ngọn lửa thắp sáng linh hồn ngươi...Và dẫn nó tan thành tro bụi..."));
-               }
-               Executor.incrementStatistic(Statistic.PLAYER_KILLS, deceased);
-            }
-         }); Scheduler.runTask(Host, new Phoebus());
-         Player.sendMessage(ChatColor.translateAlternateColorCodes('&',
-            "&6Light &8&l| &fLinh hồn ánh sáng đã cho ngươi sức mạnh, kẻ thù đã bị suy yếu..."));
-         return 0;
-      } else throw "&oKhông đủ năng lực để triển khai kĩ năng...";
-   } catch(err) {
-      return "&6Light &8&l| &cLỗi: &f" + err.message;
-   }
+  try {
+    if(DevMode)
+      throw "Kĩ năng hiện đang cập nhật! Không dùng được nhé &b:3";
+    else {
+      if(MyItems == null || PvPManager == null)
+        throw "&fMáy chủ hiện thiếu plugin &c:( &fKhông thể triển khai skill...";
+      var StatsControl = MyItems.getPlayerManager().getPlayerItemStatsManager();
+      var PvPHandler = PvPManager.getPlayerHandler(); var Matches = new ArrayList();
+      Server.getOnlinePlayers().stream().filter(function(p) {
+        if(p.equals(Player)) return false;
+        var pL = p.getLocation(); var mL = Player.getLocation();
+        if(!p.getWorld().equals(Player.getWorld())) return false;
+        else return Light.checkPvPStatus(p, PvPHandler.get(p)) && pL.distance(mL) <= Light._SKILL_RADIUS_; 
+      }).forEach(function(e) { Matches.add(e) });
+      if(Light.checkPvPStatus(Player, PvPHandler.get(Player))) {
+        var InfernoSkillTask = Java.extend(Runnable, {
+          run: function() {
+            try {
+              Light.handleEffect(Particle.END_ROD, Player.getLocation(), 20);
+              var Offensive = StatsControl.getItemStatsWeapon(Player);
+              var PVP = Offensive.getTotalPvPDamage(); var PVE = Offensive.getTotalPvEDamage();
+              var BaseDMG = Offensive.getTotalDamageMax();
+              Matches.stream().forEach(function(e) {
+                e.setVelocity(new Vector(0, 3, 0)); var Sight = Light.checkSighting(e);
+                var distance = Player.getLocation().distance(e.getLocation());
+                var Health = e.getHealth();
+                var baseFlare = Light.calc(Health, PVE);
+                baseFlare = Light.cdist(baseFlare, distance); e.setFireTicks(600);
+                var p1 = new PotionEffect(PotionEffectType.WITHER, 5, 300);
+                var p2 = new PotionEffect(PotionEffectType.BLINDNESS, 5, 300);
+                p1.apply(e); p2.apply(e);
+                e.sendMessage(Light.color("&eLight &8&l| &f&oTrong cơn sốc, ngươi có nhìn được định mệnh?..."));
+                if(e.getHealth() < baseFlare) e.setHealth(0);
+                else e.setHealth(e.getHealth() - baseFlare);
+                if(Sight) {
+                  e.sendMessage(Light.color("&eLight &8&l| &f&oThời khắc đã đến, hình phạt đã quyết..."));
+                  var Knock = Light.calc(BaseDMG, PVP);
+                  World.strikeLightningEffect(e.getLocation());
+                  if(Knock > e.getHealth()) e.setHealth(0); else e.setHealth(1);
+                }
+              }); Player.sendMessage(Light.color("&eLight &8&l| &f&oMọi thứ kết thúc...tan biến hoàn toàn..."));
+            } catch(e) { Player.sendMessage(e.toString()); }
+          }
+        }); Scheduler.runTaskLater(Host, new InfernoSkillTask(), new java.lang.Long(10)); return 1;
+      } else Player.sendMessage(Light.color("&eLight &8&l| &f&oKhông thể triển phép nơi không phải trận địa..."));
+    } return -1;
+  } catch(err) {
+    return "&eLight &8&l| &cLỗi: &f" + err;
+  }
 }
 main();
